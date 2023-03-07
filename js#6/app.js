8a635fae6ab5b0589148621af2212db8b87437c3
irakliTSKH <tskhelishvili.irakli@gmail.com>
2023-03-08 02:27:22 +0400

    lecture 6

---
+++ js#6/app.js
@@ -0,0 +1,38 @@
+// დავალება #1 (ფუნქცია რომელიც ჩაანაცვლებს სიტყვას)
+
+// const replaceFunc = (string, valueToReplace, valueToReplaceWith) => {
+//     let newStringArr = "";
+//     let arrString = string.split(" ");
+//        for(word of arrString){
+//           if(word === valueToReplace){
+//            word = valueToReplaceWith
+//          } 
+//          newStringArr += " " + word;
+//    }
+//    return newStringArr;
+// }
+
+
+
+
+// დავალება #2 (ფუნქცია აბრუნებს დიდი ასოებით სტრინგს)
+
+// const upperCaseString = (str) =>  str.toUpperCase()
+
+
+
+
+// დავალება #3 (აბრუნებს მასივს ასაკის ზრდის მიხედვით)
+
+// const users = [
+//     { 
+//       name: "Lasha",
+//       age: 30 
+//       },
+//     {
+//       name: "Saba",
+//       age: 20,
+//      }
+// ]
+
+// const sortAge = (users) => users.sort((a,b) => a.age - b.age)
