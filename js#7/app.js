08ae4a60a587b7b44d5fab5c817de6f27fe6069b
irakliTSKH <tskhelishvili.irakli@gmail.com>
2023-03-08 12:54:51 +0400

    lecture 7

---
+++ js#7/app.js
@@ -0,0 +1,31 @@
+// დავალება #1 (დაიმალოს ტექსტი)
+
+// const btn = document.querySelector("#hider");
+// const txt = document. querySelector("#text");
+// btn.addEventListener("click", () => txt.style.display = "none")
+
+
+
+
+// დავალება #2 და #3 (ელემენტების შექმნა და ფერის შეცვლა)
+
+// const createElements = () => {
+//     const cardDiv = document.createElement("div");
+//     const h2 = document.createElement("h2");
+//     const prof = document.createElement("a");
+//     const btn2 = document.createElement("button");
+//     const body = document.querySelector("body");
+
+//     body.appendChild(cardDiv);
+//     body.appendChild(h2);
+//     body.appendChild(prof);
+//     body.appendChild(btn2).innerHTML = "ClickForChange";
+
+
+//     btn2.addEventListener("click", () => {
+//         cardDiv.style.width = "100px";
+//         cardDiv.style.height = "100px";
+//         cardDiv.style.background = "red";
+//     })
+// }
+
